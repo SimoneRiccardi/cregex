f8575f149e86f9899dda7138d970ac192734f3fa
SR <>
2023-12-28 20:57:24 +0100

    adjusted documentation

--- cregex.h
+++ cregex.h
@@ -14,9 +14,7 @@
    @param  obj the object to initialize
    @param  parse_buffer array of elements used for parsing the regex
    @param  parse_buffer_size  size of parse_buffer
-   @param  read_buffer        character buffer that will be used for reading
-   @param  read_buffer_size   size of read_buffer
-   @return     initalized object
+   @return initalized object
  */
 cregex_t*  cregex_init(cregex_t* obj,cregex_element_t* parse_buffer,size_t parse_buffer_size);
 
@@ -33,17 +31,25 @@ typedef struct{
 }cregex_parse_result_t;
 
 /**
-   
-   @param obj the object where the parse result will be memorized
-   @param regex the regular exp
-   @return     initalized objectression
+   @param  obj the object where the parse result will be memorized
+   @param  regex the regular exp
    @return parse result
-   @see cregex_parse_result_t
+   @see    cregex_parse_result_t
  */
 cregex_parse_result_t    cregex_parse_str(cregex_t* obj,const char* regex);
 
+/**
+   @param   obj the object to initalize
+   @param   parsed_regex the regex to use for matching 
+   @param   file the file to read
+   @param   read_limit the maximun number of character that should be readed. After reading 'read_limit' characters the function returns  with status CREGEX_READ_REACHED_LIMIT. If no limit is needed set the variable to 0
+   @param   read_buffer        character buffer that will be used for reading
+   @param   read_buffer_size   size of read_buffer
+   @see     cregex_init
+   @return  initalized object
+ */
 
-cregex_reader_file_t*  cregex_reader_file_init(cregex_reader_file_t* obj,const cregex_t* parsed_cregex, FILE* file,size_t read_limit,char* read_buffer,size_t read_buffer_size);
+cregex_reader_file_t*  cregex_reader_file_init(cregex_reader_file_t* obj,const cregex_t* parsed_regex, FILE* file,size_t read_limit,char* read_buffer,size_t read_buffer_size);
 
 typedef enum{
     CREGEX_READ_EOF,
@@ -58,11 +64,9 @@ typedef struct{
 }cregex_read_result_t;
 
 /**
-   
-   @param obj the object that contains the parse regex and read data
-   @param file the file to 
-   @param read_limit the maximun number of character that should be readed. After reading 'read_limit' characters the function returns  with status CREGEX_READ_REACHED_LIMIT. If no limit is needed set the variable to 0
+   @param obj read settings 
    @return read result
+   @see cregex_reader_file_init
    @see cregex_read_result_t
    @see cregex_read_result_status_t
  */
