ca514b896afbde20d6c0aaa894358715d1d0abf1
SR <>
2023-12-30 14:49:04 +0100

    fix compile

--- compile_debug.sh
+++ compile_debug.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-gcc ./src/*.c ./debug/*.c  -o ./debug/a.out -std=c89 -Wall -Wextra -pedantic
-
-chmod +x ./debug/a.out
-./debug/a.out
+gcc ./src/*.c ./debug/*.c  -o ./debug/a.out -std=c89 -Wall -Wextra -pedantic -pedantic-errors &&
+chmod +x ./debug/a.out &&
+./debug/a.out 
