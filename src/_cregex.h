39b979f98eb86316e174ac9b76acc21c115a67c9
SR <>
2023-12-30 15:21:15 +0100

    definition on cregex_element_t

--- src/_cregex.h
+++ src/_cregex.h
@@ -4,9 +4,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef enum{
+    CREGEX_ELEMENT_TYPE_NONE,
+    CREGEX_ELEMENT_TYPE_RANGE,
+    CREGEX_ELEMENT_TYPE_STR,
+    CREGEX_ELEMENT_TYPE_GROUP
+}cregex_element_type_t;
+
 typedef struct{
-    
+    cregex_element_type_t _type;
+    union{
+        struct{
+            const char*     value;
+            size_t          size;
+        }str;
+        struct{
+            char        begin,end;
+        }range;
+        struct{
+            size_t size;
+        }group;
+    }_data;
+    struct{
+        size_t min;
+        size_t max;
+    }_repeat;
+
+    size_t   _or_next;
+    /*TODO ANCHOR*/
 }cregex_element_t;
+
+cregex_element_t* cregex_element_init_range(cregex_element_t* obj,char begin,char end);
+cregex_element_t* cregex_element_init_str(cregex_element_t* obj,const char* str, size_t strsize);
+cregex_element_t* cregex_element_set_repeat(cregex_element_t* obj,size_t min,size_t max);
+cregex_element_t* cregex_element_set_groupsize(cregex_element_t* obj,size_t groupsize);
 
 typedef struct{
     cregex_element_t* _parse_buffer;
