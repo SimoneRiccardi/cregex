df56a37dc8165845227d621f8a8f561e6984917b
SR <>
2023-12-30 15:25:38 +0100

    add cregex element function implementation

---
+++ src/_cregex.c
@@ -0,0 +1,32 @@
+#include "./_cregex.h"
+
+
+cregex_element_t* cregex_element_init(cregex_element_t* obj){
+    cregex_element_set_repeat(obj,1,1);
+    obj->_or_next = 0;
+    return obj;
+}
+
+cregex_element_t* cregex_element_init_range(cregex_element_t* obj,char begin,char end){
+    cregex_element_init(obj);
+    obj->_type = CREGEX_ELEMENT_TYPE_RANGE;
+    obj->_data.range.begin=begin;
+    obj->_data.range.end=end;
+    return obj;
+}
+cregex_element_t* cregex_element_init_str(cregex_element_t* obj,const char* str, size_t strsize){
+    cregex_element_init(obj);
+    obj->_type = CREGEX_ELEMENT_TYPE_STR;
+    obj->_data.str.size=strsize;
+    obj->_data.str.value=str;
+    return obj;
+}
+cregex_element_t* cregex_element_set_repeat(cregex_element_t* obj,size_t min,size_t max){
+    obj->_repeat.min=min;
+    obj->_repeat.max=max;
+    return obj;
+}
+void cregex_element_set_next_or_pos(cregex_element_t* obj,size_t pos){
+    obj->_or_next = pos;
+}
+
