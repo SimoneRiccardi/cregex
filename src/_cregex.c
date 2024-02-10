79b97723641c3635a0694df46904f588d93ac5cb
SR <>
2024-02-10 23:15:50 +0100

    Changed or paradigm

--- src/_cregex.c
+++ src/_cregex.c
@@ -2,7 +2,6 @@
 
 cregex_element_t* cregex_element_init(cregex_element_t* obj){
     cregex_element_set_repeat(obj,1,1);
-    obj->_or_next = 0;
     return obj;
 }
 
@@ -13,6 +12,13 @@ cregex_element_t* cregex_element_init_range(cregex_element_t* obj,char begin,cha
     obj->_data.range.end=end;
     return obj;
 }
+cregex_element_t* cregex_element_init_array(cregex_element_t* obj,cregex_element_array_type_t type, size_t size){
+    cregex_element_init(obj);
+    obj->_type = CREGEX_ELEMENT_TYPE_ARRAY;
+    obj->_data.array.type=type; 
+    obj->_data.array.size=size;
+    return obj;
+}
 cregex_element_t* cregex_element_init_str(cregex_element_t* obj,const char* str, size_t strsize){
     cregex_element_init(obj);
     obj->_type = CREGEX_ELEMENT_TYPE_STR;
@@ -20,13 +26,21 @@ cregex_element_t* cregex_element_init_str(cregex_element_t* obj,const char* str,
     obj->_data.str.value=str;
     return obj;
 }
+
+size_t cregex_element_array_get_size(cregex_element_t *obj){
+    return obj->_data.array.size;
+}
+void cregex_element_array_set_size(cregex_element_t *obj,size_t s){
+    obj->_data.array.size=s;
+}
+void cregex_element_array_add_size(cregex_element_t *obj,int s){
+    obj->_data.array.size+=s;
+}
+
 cregex_element_t* cregex_element_set_repeat(cregex_element_t* obj,size_t min,size_t max){
     obj->_repeat.min=min;
     obj->_repeat.max=max;
     return obj;
-}
-void cregex_element_set_next_or_pos(cregex_element_t* obj,size_t pos){
-    obj->_or_next = pos;
 }
 
 bool              cregex_element_is_repeat_max_infinite(cregex_element_t *obj){
--- src/_cregex.h
+++ src/_cregex.h
@@ -11,8 +11,13 @@ typedef enum{
     CREGEX_ELEMENT_TYPE_NONE,
     CREGEX_ELEMENT_TYPE_RANGE,
     CREGEX_ELEMENT_TYPE_STR,
-    CREGEX_ELEMENT_TYPE_GROUP
+    CREGEX_ELEMENT_TYPE_ARRAY
 }cregex_element_type_t;
+
+typedef enum{
+    CREGEX_ELEMENT_ARRAY_TYPE_GROUP,
+    CREGEX_ELEMENT_ARRAY_TYPE_OR
+}cregex_element_array_type_t;
 
 typedef struct{
     cregex_element_type_t _type;
@@ -26,20 +31,21 @@ typedef struct{
         }range;
         struct{
             size_t size;
-        }group;
+            cregex_element_array_type_t type;
+        }array;
     }_data;
     struct{
         size_t min;
         size_t max;
     }_repeat;
-
-    size_t   _or_next;
     /*TODO ANCHOR*/
 }cregex_element_t;
 
 cregex_element_t* cregex_element_init_range(cregex_element_t* obj,char begin,char end);
 cregex_element_t* cregex_element_init_str(cregex_element_t* obj,const char* str, size_t strsize);
-void              cregex_element_set_next_or_pos(cregex_element_t* obj,size_t pos);
+cregex_element_t* cregex_element_init_array(cregex_element_t* obj,cregex_element_array_type_t type, size_t size);
+
+size_t cregex_element_array_get_size(cregex_element_t *obj);
 
 cregex_element_t* cregex_element_set_repeat(cregex_element_t* obj,size_t min,size_t max);
 /*only valid if cregex_element_is_repeat_max_infinite(obj)==false*/
